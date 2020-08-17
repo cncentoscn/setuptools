8519a8ecb46ffec0f333540aa25464b9ba450cfa
lucky <lucky@centoscn.vip>
2020-08-17 19:58:06 +0800

    。

--- scripts/install_gitlab.sh
+++ scripts/install_gitlab.sh
@@ -12,15 +12,30 @@ else
   echo -e "[\033[31m ERROR \033[0m] 请用 root 用户执行安装脚本"
   flag=1
 fi
-echo
-cat << EOF
-     _______ __  __          __
-    / ____(_) /_/ /   ____ _/ /_
-   / / __/ / __/ /   / __ `/ __ \
-  / /_/ / / /_/ /___/ /_/ / /_/ /
-  \____/_/\__/_____/\__,_/_.___/
-EOF
+#CPU检测
+echo -ne "CPU2    Check \t........................ "
+processor=`cat /proc/cpuinfo| grep "processor"| wc -l`
+if [ $processor -lt 2 ];then
+  echo -e "[\033[31m ERROR \033[0m] CPU 小于 2核，JumpServer 所在机器的 CPU 需要至少 2核"
+  flag=1
+else
+  echo -e "[\033[32m OK \033[0m]"
+fi
 
+#内存检测
+echo -ne "Memory4 Check \t........................ "
+memTotal=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
+if [ $memTotal -lt 3750000 ];then
+  echo -e "[\033[31m ERROR \033[0m] 内存小于 4G，JumpServer 所在机器的内存需要至少 4G"
+  flag=1
+else
+  echo -e "[\033[32m OK \033[0m]"
+fi
+
+if [ $flag -eq 1 ]; then
+  echo "安装环境检测未通过，请查阅上述环境检测结果"
+  exit 1
+fi
 #安装基础依赖
     which policycoreutils-python >/dev/null 2>&1
     if [ $? -ne 0 ];then
