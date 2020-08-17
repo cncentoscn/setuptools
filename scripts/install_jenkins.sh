3416bc565ac76a1a747102e0a3cadcf3ef0f358b
lucky <lucky@centoscn.vip>
2020-08-17 20:43:27 +0800

    no message

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -3,28 +3,27 @@
 # Copyright (c) 2020
 # Gmail: lucky@centoscn.vip
 # blog:  www.centoscn.vip
+which wget >/dev/null 2>&1
+if [ $? -ne 0 ];then
+    yum install -y wget
+fi
 #下载yum源
-cat >> /etc/yum.repos.d/jenkins.repo <<EOF
-[jenkins]
-name=Jenkins
-baseurl=http://pkg.jenkins.io/redhat
-gpgcheck=1
-EOF
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 #安装key
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 #安装基础服务
-    which upgrade >/dev/null 2>&1
-    if [ $? -ne 0 ];then
-        yum install -y upgrade
-    fi
-    which jenkins >/dev/null 2>&1
-    if [ $? -ne 0 ];then
-        yum install -y jenkins
-    fi
-    which java-1.8.0-openjdk-devel >/dev/null 2>&1
-    if [ $? -ne 0 ];then
-        yum install -y java-1.8.0-openjdk-devel
-    fi
+which upgrade >/dev/null 2>&1
+if [ $? -ne 0 ];then
+    yum install -y upgrade
+fi
+which jenkins >/dev/null 2>&1
+if [ $? -ne 0 ];then
+    yum install -y jenkins
+fi
+which java-1.8.0-openjdk-devel >/dev/null 2>&1
+if [ $? -ne 0 ];then
+    yum install -y java-1.8.0-openjdk-devel
+fi
 #启动服务
 systemctl start jenkins
 systemctl enable  jenkins
